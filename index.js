8bea544c4bbc920fa960ad024e8ec92c4f1c4812
vaguele <115198189+vaguele@users.noreply.github.com>
2024-09-05 19:21:44 -0700

    Flip Cards

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function initializeNewGame(){
 function startRound(){
     initializeNewRound()
     collectCards()
+    flipCards(true)
 }
 
 function initializeNewRound(){
@@ -51,6 +52,24 @@ function addCardsToGridAreaCell(cellPositionClassName){
     const cellPositionElem = document.querySelector(cellPositionClassName)
     cards.forEach((card, index) => {
         addChildElement(cellPositionElem, card)
+    })
+}
+
+function flipCard(card, flipToBack){
+    const innerCardElem = card.firstChild
+    if(flipToBack && !innerCardElem.classList.contains('flip-it')){
+        innerCardElem.classList.add('flip-it')
+    }
+    else if(innerCardElem.classList.contains('flip-it')){
+        innerCardElem.classList.remove('flip-it')
+    }
+}
+
+function flipCards(flipToBack){
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            flipCard(card, flipToBack)
+        }, index * 100)
     })
 }
 
