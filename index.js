a283e7081d19ba69616ea431b9750babb6bb49e3
vaguele <115198189+vaguele@users.noreply.github.com>
2024-09-06 19:48:00 -0700

    Deal Cards

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ function initializeNewGame(){
 function startRound(){
     initializeNewRound()
     collectCards()
-    flipCards(true)
+    //flipCards(true)
+    shuffleCards()
 }
 
 function initializeNewRound(){
@@ -84,6 +85,7 @@ function shuffleCards(){
         
         if(shuffleCount == 500){
             clearInterval(id)
+            dealCards()
         }
         else{
             shuffleCount++;
@@ -99,6 +101,48 @@ function randomizeCardPosition(){
 
     cardPositions[random1 - 1] = cardPositions[random2 - 1]
     cardPositions[random2 - 1] = temp
+}
+
+function dealCards(){
+    addCardsToAppropriateCell()
+    const areasTemplate = returnGridAreasMappedToCardPos()
+    transformGridArea(areasTemplate)
+}
+
+function returnGridAreasMappedToCardPos(){
+    let firstPart = ""
+    let secondPart = ""
+    let areas = ""
+
+    cards.forEach((card, index) => {
+        if(cardPositions[index] == 1){
+            areas = areas + "a "
+        }
+        else if(cardPositions[index] == 2){
+            areas = areas + "b "
+        }
+        else if(cardPositions[index] == 3){
+            areas = areas + "c "
+        }
+        else if(cardPositions[index] == 4){
+            areas = areas + "d "
+        }
+        if(index == 1){
+            firstPart = areas.substring(0, areas.length - 1)
+            areas = ""
+        }
+        else if(index == 3){
+            secondPart = areas.substring(0, areas.length - 1)
+        }
+    })
+
+    return `"${firstPart}" "${secondPart}"`
+}
+
+function addCardsToAppropriateCell(){
+    cards.forEach((card) => {
+        addCardToGridCell(card)
+    })
 }
 
 function createCards(){
