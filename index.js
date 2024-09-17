b0d928662aa6738cf364aea9abc8cfab50fcfc57
vaguele <115198189+vaguele@users.noreply.github.com>
2024-09-17 11:11:58 -0700

    Animation

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ function canChooseCard(){
 function loadGame(){
     createCards()
     cards = document.querySelectorAll('.card')
+    cardFlyInEffect()
     playGameButtonElem.addEventListener('click', ()=>startGame())
 
     updateStatusElement(scoreContainerElem, "none")
@@ -205,16 +206,60 @@ function flipCards(flipToBack){
     })
 }
 
+function cardFlyInEffect(){
+    const id = setInterval(flyIn, 5)
+    let cardCount = 0;
+    let count = 0
+
+    function flyIn(){
+        count++
+        if (cardCount == numCards){
+            clearInterval(id)
+            playGameButtonElem.style.display = "inline-block"
+        }
+        if (count == 1 || count == 250 || count == 500 || count == 750){
+            cardCount++
+            let card = document.getElementById(cardCount)
+            card.classList.remove("fly-in")
+        }
+    }
+}
+
+function removeShuffleClasses(){
+    cards.forEach((card) => {
+        card.classList.remove("shuffle-left")
+        card.classList.remove("shuffle-right")
+    })
+}
+
+function animateShuffle(shuffleCount){
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    let card1 = document.getElementById(random1)
+    let card2 = document.getElementById(random2)
+
+    if (shuffleCount % 4 == 0){
+        card1.classList.toggle("shuffle-left")
+        card1.style.zIndex = 100
+    }
+    if (shuffleCount % 10 == 0){
+        card2.classList.toggle("shuffle-left")
+        card2.style.zIndex = 200
+    }
+}
+
 function shuffleCards(){
     const id = setInterval(shuffle, 12)
     let shuffleCount = 0
 
     function shuffle(){
         randomizeCardPosition()
-        
+        animateShuffle(shuffleCount)
         if(shuffleCount == 500){
             clearInterval(id)
             shufflingInProgress = false
+            removeShuffleClasses()
             dealCards()
             updateStatusElement(currentGameStatusElem, "block", primaryColor, "Try to find the Ace of Spades")
         }
@@ -295,6 +340,7 @@ function createCard(cardItem){
 
     //  add class and id to card element
     addClassToElement(cardElem, 'card')
+    addClassToElement(cardElem, 'fly-in')
     addIdToElement(cardElem, cardItem.id)
 
     // add class to inner card element
