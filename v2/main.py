04aa34c45f3a06af4d6a6567c9fb4f0e94a7576f
vaguele <115198189+vaguele@users.noreply.github.com>
2025-04-22 01:36:06 -0700

    Leading Suit Established
    Determine which card wins based on the leading suit and trump next

--- v2/main.py
+++ v2/main.py
@@ -1,15 +1,15 @@
 import random
 
 class Card:
-    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    type = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    weight = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
     
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
 
     def strength(self):
-        return Card.values[self.value]
+        return Card.weight[self.value]
 
     def __str__(self):
         return f"{self.value} of {self.suit}"
@@ -20,8 +20,8 @@ class Card:
 class Deck:
     def __init__(self):
         self.cards = []
-        for suit in Card.suits:
-            for value in Card.values:
+        for suit in Card.type:
+            for value in Card.weight:
                 self.cards.append(Card(suit, value))
         self.shuffle()
 
@@ -54,14 +54,14 @@ class Player:
     def place_bid(self):
         self.bid = int(input(f"\nHow much does {self.name} wanna bid? "))
 
-    def play_card(self):
+    def play_card(self, leading_suit):
         choice = {}
         for i in range(len(self.hand)):
             choice[i+1] = self.hand[i]
         
         print(f"\n{choice}")
         play = int(input(f"\nWhich card would {self.name} like to play? "))
-        self.played_cards = self.hand.pop(play - 1)
+        self.played_card = self.hand.pop(play - 1)
         print(f"\n{self.name}'s new hand: {self.hand}")
 
     def round_reset(self):
@@ -70,7 +70,7 @@ class Player:
         self.hand = []
 
     def __str__(self):
-        return f"\n{Player.name} played "
+        return f"\n{self.name}'s score is {self.score} {self.hand} "
     
     def __repr__(self):
         return self.__str__()
@@ -79,16 +79,23 @@ class Player:
 if __name__ == "__main__":
     #num_players = int(input("Please enter the number of players"))
     players  = [Player("Jake"), Player("Finn")]
-    
+    cards_per_player = 3
+
     """
     for _ in range(num_players):
         players.append(Player(input("Enter Name: ")))
     """
 
     deck = Deck()
-    hands = deck.deal(players, cards_per_player=3)
+    deck.deal(players, cards_per_player)
+    """"
+    print(players[0])
+    print(players[0].hand[0].weight[players[0].hand[0].value])
+    """
+
     trump = deck.reveal_trump()
-    leading_suit = ""
+    leading_suit = None
+    winner  = ""
 
     for player in players:
         print(f"\n{player.name}'s hand: {player.hand}")
@@ -102,14 +109,15 @@ if __name__ == "__main__":
         print(f"\nNO TRUMP")
 
     # Check point for later, 
-    # - Establish leading suit
     # - Determine which card wins based on the leading suit and trump
     # - Update tricks won, update player score at the end
 
-    for i in range(2):
+    for i in range(cards_per_player):
         for player in players:
-            player.play_card()
-    
+            player.play_card(leading_suit)
+            if leading_suit == None:
+                leading_suit = player.played_card.suit
+                print(f"\n{leading_suit} lead")
 
     for player in players:
         player.round_reset()
